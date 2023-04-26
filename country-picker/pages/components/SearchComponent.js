b83a824f838baf4c807044dd4ed4fe6a63d65c6c
Antonio <globaltailwind@protonmail.com>
2023-04-26 06:51:05 -0300

    added some basic format

---
+++ country-picker/pages/components/SearchComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+function SearchComponent() {
+
+
+  return (
+
+    <div className ="box-border h-40 w-6/12 p-4 border-solid border-2 border-black flex items-center justify-center">
+
+<button className="box-border h-7 w-20 border-solid border-2 border-black"
+>Press Me
+</button>
+
+
+    </div>
+  )
+}
+
+export default SearchComponent
--- country-picker/pages/index.js
+++ country-picker/pages/index.js
@@ -1,14 +1,11 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import SearchComponent from './components/SearchComponent'
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1>Hello World</h1>
+    <main className="flex justify-center items-center h-screen">
+      <SearchComponent/>
     </main>
+     
   )
 }
